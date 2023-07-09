263db245ef3c9a42392888b591e2a33a265cbf2a
Hamza <hamza.sulaiman2009@gmail.com>
2023-07-09 19:43:31 +0200

    'PeformanceImprovement'

--- holistics.py
+++ holistics.py
@@ -1,15 +1,22 @@
+import threading
+import matplotlib.pyplot as plt
 import mediapipe as mp
 import cv2
-
-REGIONS = [4, 16, 20, 8, 12]
-MOVE_CURSOR_CONN = [1, 1, 1, 0, 0]
-CLICK_CURSOR_CONN = [0, 1, 1, 0, 0]
+import numpy as np
+import pyautogui as py
 
 
 class HandTrack:
-    def __init__(self,
-                 mindetectconf=0.5,
-                 mintrackconf=0.5):
+    REGIONS = [4, 16, 20, 8, 12]
+    MOVE_CURSOR_CONN = [1, 1, 1, 0, 0]
+    LT_CLICK_CURSOR_CONN = [0, 1, 1, 0, 0]
+    RT_CLICK_CURSOR_CONN = [1, 1, 1, 0, 1]
+    SCROLL_START = [0, 1, 1, 1, 1]
+    SCROLL_END = [0, 1, 1, 0, 1]
+    prev_gesture = []
+    lt_timer = 0
+
+    def __init__(self, mindetectconf=0.5, mintrackconf=0.5):
 
         self.mindetectconf = mindetectconf
         self.mintrackconf = mintrackconf
@@ -26,38 +33,123 @@ class HandTrack:
                                                      min_detection_confidence=self.mindetectconf,
                                                      min_tracking_confidence=self.mintrackconf,
                                                      )
+        self.width, self.height = py.size()
 
-    def trackhands(self, image, draw=True):
-        self.h, self.w, _ = image.shape
-        left = None
-        right = None
-        image = cv2.flip(image, 1)  ## cause left hand to be right hand and vice versa
-        frame = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        results = self.both_hands.process(frame)
+    def trackhands(self, frame, interact=True, draw=True):
 
+        self.image = self.__preprocess(frame)
+        cv2.putText(self.image, 'Move', (551, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(self.image, 'Click', (551, 55), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+        cv2.putText(self.image, 'Scroll Start', (551, 85), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2,
+                    cv2.LINE_AA)
+
+        results = self.both_hands.process(self.image)
+        self.image = cv2.cvtColor(self.image, cv2.COLOR_RGB2BGR)
         if results.left_hand_landmarks:
-
-            left = results.left_hand_landmarks
             if draw:
-                self.mp_drawing.draw_landmarks(image, results.left_hand_landmarks, self.mp_holistics.HAND_CONNECTIONS,
+                self.mp_drawing.draw_landmarks(self.image, results.left_hand_landmarks,
+                                               self.mp_holistics.HAND_CONNECTIONS,
                                                self.mp_drawing.DrawingSpec((255, 255, 51)),
                                                )
 
         if results.right_hand_landmarks:
-            right = results.right_hand_landmarks
-
             if draw:
-                self.mp_drawing.draw_landmarks(image, results.right_hand_landmarks, self.mp_holistics.HAND_CONNECTIONS,
+                self.mp_drawing.draw_landmarks(self.image, results.right_hand_landmarks,
+                                               self.mp_holistics.HAND_CONNECTIONS,
                                                self.mp_drawing.DrawingSpec((255, 255, 51)),
                                                )
 
-        return left, right, image
+        if not interact:
+            return self.image
 
-    def get_landmarks(self, handlandmark):
-        if handlandmark is not None:
-            lm = {}
-            for id, lmark in enumerate(handlandmark.landmark):
-                cx, cy = int(lmark.x * self.w), int(lmark.y * self.h)
-                lm[id] = [cx, cy, lmark.z]
-            return lm
+        else:
+            if results.left_hand_landmarks or results.right_hand_landmarks:
+                self.__interact(results.left_hand_landmarks, results.right_hand_landmarks)
 
+            return self.image
+
+    def __interact(self, right, left):
+        if left:
+            HandTrack.lt_timer = self.__enable_timer(HandTrack.lt_timer)
+            left_handf_coordinate, bbox = self.__getxypos(left)
+
+            cv2.rectangle(self.image, (bbox[0]-15, bbox[1]-15), (bbox[2]+15, bbox[3]+15), (255, 255, 51), 5)
+            enc = self.__get_lm_list(left_handf_coordinate)
+
+            xcord = int(np.mean([left_handf_coordinate[8][0], left_handf_coordinate[12][0]]))
+            ycord = int(np.mean([left_handf_coordinate[8][1], left_handf_coordinate[12][1]]))
+            xcord = np.interp(xcord, [0, self.image.shape[1]], [0, self.width + 400])
+            ycord = np.interp(ycord, [0, self.image.shape[0]], [0, self.height + 400])
+
+            if enc == HandTrack.MOVE_CURSOR_CONN:
+                cv2.putText(self.image, 'Move', (551, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
+                t1 = threading.Thread(target=self.__moveThread, args=[xcord, ycord])
+                t1.start()
+
+            if enc == HandTrack.LT_CLICK_CURSOR_CONN:
+                if HandTrack.lt_timer == 0:
+                    cv2.putText(self.image, 'Click', (551, 55), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2,
+                                cv2.LINE_AA)
+
+                    t2 = threading.Thread(target=self.__clickThread, args=[xcord, ycord])
+                    t2.start()
+                    HandTrack.lt_timer = 5
+
+            if enc == HandTrack.SCROLL_START:
+                HandTrack.prev_gesture = HandTrack.SCROLL_START
+                cv2.putText(self.image, 'Scroll Start', (551, 85), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2,
+                            cv2.LINE_AA)
+            if (HandTrack.prev_gesture == HandTrack.SCROLL_START) and (enc == HandTrack.SCROLL_END):
+                cv2.putText(self.image, 'Scroll Start', (551, 85), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2,
+                            cv2.LINE_AA)
+                t3 = threading.Thread(target=self.__scrollThread)
+                t3.start()
+
+    def __moveThread(self, xcord, ycord):
+        py.moveTo(xcord, ycord, 0, py.easeOutQuad)
+
+    def __clickThread(self, xcord, ycord):
+        py.click(x=xcord, y=ycord, button='LEFT')
+
+    def __scrollThread(self):
+        py.vscroll(-100)
+
+    def __getxypos(self, landmarks):
+        lm = {}
+        fingerListx = []
+        fingerListy = []
+        for id, lmark in enumerate(landmarks.landmark):
+            cx, cy = int(lmark.x * self.h), int(lmark.y * self.w)
+            fingerListx.append(cx)
+            fingerListy.append(cy)
+            lm[id] = [cx, cy, lmark.z]
+        bbox = [min(fingerListx), min(fingerListy), max(fingerListx), max(fingerListy)]
+        return lm, bbox
+
+    def __preprocess(self, frame):
+        self.w, self.h, _ = frame.shape
+        frame = cv2.flip(frame, 1)
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+        return frame
+
+    def __enable_timer(self, timer):
+        if timer > 0:
+            timer -= 1
+            return timer
+        else:
+            return 0
+
+    def __get_lm_list(self, leftlandmark):
+        x = []
+        if leftlandmark[HandTrack.REGIONS[0]][0] < leftlandmark[HandTrack.REGIONS[0] - 1][0]:  # thumb closed
+            x.append(1)
+        else:
+            x.append(0)
+
+        for reg in HandTrack.REGIONS[1:]:
+
+            if leftlandmark[reg][1] > leftlandmark[reg - 2][1]:  # fingers closed
+                x.append(1)
+            else:
+                x.append(0)
+        return x
--- test.py
+++ test.py
@@ -1,54 +1,33 @@
 import cv2
 import numpy as np
-import holistics
 from holistics import HandTrack
 import pyautogui as py
 import time
 
-handtrack = HandTrack(mindetectconf=0.7, mintrackconf=0.7)
+handtrack = HandTrack(mindetectconf=0.3, mintrackconf=0.3)
 width, height = py.size()
 
 py.FAILSAFE = False
 
 cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
-
+TIMER = 0
+fps_start_time = 0
+fps = 0
 while cap.isOpened():
+
     ret, frame = cap.read()
 
-    right, left, frame = handtrack.trackhands(image=frame, draw=True)
-    cv2.putText(frame, 'Move', (551, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-    cv2.putText(frame, 'Click', (551, 55), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2, cv2.LINE_AA)
-    left_lmark = handtrack.get_landmarks(left)
-    right_lmark = handtrack.get_landmarks(right)
+    frame = handtrack.trackhands(frame=frame, interact=True, draw=True)
 
-    if left_lmark:
-        x = []
-        if left_lmark[holistics.REGIONS[0]][0] < left_lmark[holistics.REGIONS[0] - 1][0]:  # thumb closed
-            x.append(1)
-        else:
-            x.append(0)
+    fps_end_time = time.time()
+    fps_diff = fps_end_time - fps_start_time
+    fps = 1 / fps_diff
+    fps_start_time = fps_end_time
 
-        for reg in holistics.REGIONS[1:]:
-
-            if left_lmark[reg][1] > left_lmark[reg - 2][1]:  # fingers closed
-                x.append(1)
-            else:
-                x.append(0)
-
-        if x == holistics.MOVE_CURSOR_CONN:
-            cv2.putText(frame, 'Move', (551, 25), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            xcord = np.mean([left_lmark[8][0], left_lmark[12][0]])
-            ycord = np.mean([left_lmark[8][1], left_lmark[12][1]])
-            xcord = np.interp(xcord, [0, frame.shape[1]], [0, width + 400])
-            ycord = np.interp(ycord, [0, frame.shape[0]], [0, height + 400])
-            py.moveTo(xcord, ycord, duration=0)
-
-        if x == holistics.CLICK_CURSOR_CONN:
-            cv2.putText(frame, 'Click', (551, 55), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2, cv2.LINE_AA)
-            py.click()
-            time.sleep(1)
+    cv2.putText(frame, str(round(fps, 2)), (5, 30), cv2.FONT_HERSHEY_COMPLEX, 1, (0, 255, 255), 1)
 
     cv2.imshow('frame', frame)
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
